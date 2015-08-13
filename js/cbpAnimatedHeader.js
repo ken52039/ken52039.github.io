3bc2b478282575fdcb9d4a6f284aefa291da91bf
ken52039 <ken52039@gmail.com>
2015-08-13 21:49:49 +0800

    Header調整設計

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -13,7 +13,7 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-fixed-top' ),
 		didScroll = false,
-		changeHeaderOn = 1;
+		changeHeaderOn = 455;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
