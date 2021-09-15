d04e92c8d546b2cb64f8a38a44125744dbb70bbe
Ken Huang <ken52039@gmail.com>
2021-09-15 23:33:22 +0800

    上傳富邦奈米投QA

---
+++ Fubon-nmplus-QA/js/sub_style_faq.js
@@ -0,0 +1,495 @@
+/*===tab=======================================*/
+function myFunctionA() {
+    var x = document.getElementById("myDIV-A");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunctionB() {
+    var x = document.getElementById("myDIV-B");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunctionC() {
+    var x = document.getElementById("myDIV-C");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunctionD() {
+    var x = document.getElementById("myDIV-D");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunctionE() {
+    var x = document.getElementById("myDIV-E");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunctionF() {
+    var x = document.getElementById("myDIV-F");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunctionG() {
+    var x = document.getElementById("myDIV-G");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunctionH() {
+    var x = document.getElementById("myDIV-H");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+/*---FAQ---*/
+function myFunctionI() {
+    var x = document.getElementById("myDIV-I");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunctionJ() {
+    var x = document.getElementById("myDIV-J");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunctionK() {
+    var x = document.getElementById("myDIV-K");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunctionL() {
+    var x = document.getElementById("myDIV-L");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunctionM() {
+    var x = document.getElementById("myDIV-M");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+} 
+
+/*===QA=======================================*/
+
+function myFunction1() {
+    var x = document.getElementById("QA-DIV1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction2() {
+    var x = document.getElementById("QA-DIV2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction3() {
+    var x = document.getElementById("QA-DIV3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction4() {
+    var x = document.getElementById("QA-DIV4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction5() {
+    var x = document.getElementById("QA-DIV5");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction6() {
+    var x = document.getElementById("QA-DIV6");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction7() {
+    var x = document.getElementById("QA-DIV7");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction8() {
+    var x = document.getElementById("QA-DIV8");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction9() {
+    var x = document.getElementById("QA-DIV9");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction10() {
+    var x = document.getElementById("QA-DIV10");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction11() {
+    var x = document.getElementById("QA-DIV11");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction12() {
+    var x = document.getElementById("QA-DIV12");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction13() {
+    var x = document.getElementById("QA-DIV13");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction14() {
+    var x = document.getElementById("QA-DIV14");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction15() {
+    var x = document.getElementById("QA-DIV15");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction16() {
+    var x = document.getElementById("QA-DIV16");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction17() {
+    var x = document.getElementById("QA-DIV17");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction18() {
+    var x = document.getElementById("QA-DIV18");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction15() {
+    var x = document.getElementById("QA-DIV15");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction19() {
+    var x = document.getElementById("QA-DIV19");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction20() {
+    var x = document.getElementById("QA-DIV20");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction21() {
+    var x = document.getElementById("QA-DIV21");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction22() {
+    var x = document.getElementById("QA-DIV22");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction23() {
+    var x = document.getElementById("QA-DIV23");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction24() {
+    var x = document.getElementById("QA-DIV24");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction24() {
+    var x = document.getElementById("QA-DIV24");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction25() {
+    var x = document.getElementById("QA-DIV25");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction26() {
+    var x = document.getElementById("QA-DIV26");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction27() {
+    var x = document.getElementById("QA-DIV27");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction28() {
+    var x = document.getElementById("QA-DIV28");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction29() {
+    var x = document.getElementById("QA-DIV29");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction30() {
+    var x = document.getElementById("QA-DIV30");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction31() {
+    var x = document.getElementById("QA-DIV31");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction32() {
+    var x = document.getElementById("QA-DIV32");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction33() {
+    var x = document.getElementById("QA-DIV33");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction34() {
+    var x = document.getElementById("QA-DIV34");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction35() {
+    var x = document.getElementById("QA-DIV35");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction36() {
+    var x = document.getElementById("QA-DIV36");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction37() {
+    var x = document.getElementById("QA-DIV37");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+function myFunction38() {
+    var x = document.getElementById("QA-DIV38");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Fubon-nmplus-QA/js/sub_style_faq_search.js
@@ -0,0 +1,112 @@
+/*
+    20210825 完成大部分功能
+    20210826 修正為未符合後台結果時不消失
+    20210829 整理邏輯
+*/
+
+$(function() {
+
+    var allCategories = $("div[id^=cat]");
+    var allCategoriesBlock = $("div[id^=cat] > div[id^=myDIV]");
+    var allQuestions = $("div[id^=myDIV] > div > button.QA");
+    var allAnswers = $("div[id^=QA-DIV]");
+
+    // 計算搜尋輸入框字數
+    function faqKeyLengthCal() {
+        $(".searchTerm").next().append(
+            '<i id="searchDelIcon" class="fas fa-times-circle" style="display: none;"></i>'
+        );
+        $("#searchKeyLength").text($(".searchTerm").val().length + "/10");
+        if ($(".searchTerm").val() != "") {
+            $("#searchDelIcon").show();
+        } else {
+            $("#searchDelIcon").hide();
+        }
+    }
+
+    // 檢查關鍵字是否合法
+    function isMatchesReg(keyword) {
+        var faqSearchKeyReg = "^[(\u4e00-\u9fa5)(a-zA-Z0-9)]{0,10}$";
+        if (keyword.length > 0 && !keyword.match(faqSearchKeyReg)) {
+            $("#searchKeyError").text("請輸入中文、英文、數字~");
+            return false;
+        }
+        $("#searchKeyError").text("");
+        return true;
+    }
+
+    // highlight plugin
+    function faqHighlightJSKey(keyword) {
+        $(allCategories).removeHighlight();
+        if (keyword.length > 0) {
+            $(allCategories).highlight(keyword);
+        }
+    }
+
+    // 篩選搜尋關鍵字，並展開答案
+    function faqSearch(keyword) {
+        
+        // 換關鍵字搜尋時，無論如何所有答案都要關閉
+        $(allAnswers).hide();
+        
+        // 若關鍵字為空，重新收合所有標籤
+        if ($(".searchTerm").val() == "") {
+            $(allCategories).show();
+            $(allCategoriesBlock).hide();
+            // $(allQuestions).show();
+            // $(allAnswers).hide();
+            return;
+        }
+        
+        allCategories.filter(function() {
+            // 類別是否有找到
+            if ( $(this).children("button").text().indexOf(keyword) > -1 ) {
+                $(this).find("div[id^=myDIV]").show();
+            } else {
+                $(this).find("div[id^=myDIV]").hide();
+            }
+        });
+
+        allQuestions.filter(function() {
+            if ($(this).text().indexOf(keyword) > -1 ) {
+                $(this).closest("div[id^=myDIV]").show();
+                $(this).next().show();
+            }
+        });
+
+        allAnswers.filter(function() {
+            if ($(this).text().indexOf(keyword) > -1 ) {
+                $(this).closest("div[id^=myDIV]").show();
+                $(this).show();
+            }
+        });
+    }
+
+    // 點搜尋按鈕，觸發關鍵字搜尋
+    $(".searchButton").on("click", function() {
+        var faqSearchKeyStr = $(".searchTerm").val();
+        if (isMatchesReg(faqSearchKeyStr)) {
+            faqSearch(faqSearchKeyStr);
+            faqHighlightJSKey(faqSearchKeyStr);
+        }
+    });
+
+    // 輸入時，觸發計算字數功能、檢查合法輸入值
+    // 按 enter 時同點擊搜尋按鈕
+    $(".searchTerm").on("keyup", function(e) {
+        faqKeyLengthCal();
+        if (isMatchesReg($(this).val())
+                && e.key === "Enter") {
+            $(".searchButton").trigger("click");
+        }
+    });
+
+    // 文字刪除(X)按鈕
+    $(".search").on("click", "#searchDelIcon", function() {
+        $(".searchTerm").val("");
+        $(allCategories).removeHighlight();
+        faqKeyLengthCal();
+        $(".searchTerm").trigger("focus");
+    });
+    
+});
